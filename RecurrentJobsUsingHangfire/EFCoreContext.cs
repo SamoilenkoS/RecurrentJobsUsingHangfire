2e3884e2a5de328e6ccbe69d238e1c39a4b3b6bb
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-13 15:52:00 +0200

    Added DB notification

---
+++ RecurrentJobsUsingHangfire/EFCoreContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecurrentJobsUsingHangfire
+{
+    public class EFCoreContext : DbContext
+    {
+        public DbSet<Good> Goods { get; set; }
+
+        public EFCoreContext(DbContextOptions<EFCoreContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ RecurrentJobsUsingHangfire/Good.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecurrentJobsUsingHangfire
+{
+    public class Good
+    {
+        public int GoodId { get; set; }
+        public string Title { get; set; }
+        public Category Category { get; set; }
+        public int MinCount { get; set; }
+    }
+}
