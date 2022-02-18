c62596a37b81be88b343682305e31a0d7d90f132
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-18 17:35:10 +0200

    Implement ManyJobs per minute option

--- RecurrentJobsUsingHangfire/MonitoringJob.cs
+++ RecurrentJobsUsingHangfire/MonitoringJob.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -8,10 +9,13 @@ namespace RecurrentJobsUsingHangfire
 {
     public class MonitoringJob
     {
+        private const int TotalJobDuration = 55;
+        private const int DelayBetweenMessages = 5;
         private static int _count;
         private EFCoreContext _dbContext;
         private IHubContext<MonitoringHub> _monitorHub;
         private ILogger<MonitoringJob> _logger;
+        private DateTime _startDateTime;
 
         public MonitoringJob(
             EFCoreContext dbContext,
@@ -21,9 +25,23 @@ namespace RecurrentJobsUsingHangfire
             _dbContext = dbContext;
             _monitorHub = monitorHub;
             _logger = logger;
+            _startDateTime = DateTime.Now;
         }
 
         public async Task StartJob()
+        {
+            _logger.LogInformation("Job run started!");
+            while(DateTime.Now.Subtract(_startDateTime).TotalSeconds < TotalJobDuration)
+            {
+                await JobBody();
+
+                await Task.Delay(TimeSpan.FromSeconds(DelayBetweenMessages));
+            }
+
+            _logger.LogInformation("Job run finished!");
+        }
+
+        private async Task JobBody()
         {
             _logger.LogInformation("Current count: {count}", ++_count);
 
